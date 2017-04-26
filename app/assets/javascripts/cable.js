7e3d2dc9006e1e870e2c39b01dada339d804723a
Autumn <abetterautumn@gmail.com>
2017-04-26 11:01:50 +0800

    change the navbar and try the carousel

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -11,3 +11,39 @@
   App.cable = ActionCable.createConsumer();
 
 }).call(this);
+
+//----------navbar----------
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
+
+//---------carousel---------
+var $item = $('.carousel .item');
+var $wHeight = $(window).height();
+$item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+});
+
+$('.carousel').carousel({
+  interval: 6000,
+  pause: "false"
+});
